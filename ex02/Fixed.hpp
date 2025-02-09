b5fb6ce42f1a4539ea3cdc80a23082fef142f154
Irene_home <puigparnaui@gmail.com>
2025-02-09 15:32:14 +0100

    ex02 starting

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FixedPoint.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 12:56:33 by ipuig-pa          #+#    #+#             */
+/*   Updated: 2025/02/06 10:55:40 by ipuig-pa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <string>
+# include <iostream>
+# include <cmath>
+
+class Fixed{
+
+public:
+	Fixed();
+	Fixed(const int value);
+	Fixed(const float value);
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &other);
+	~Fixed(void);
+
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+	float toFloat( void ) const;
+	int toInt( void ) const;
+
+	Fixed &operator>(const Fixed &first, const Fixed &second);
+	Fixed &operator<(const Fixed &first, const Fixed &second);
+	Fixed &operator>=(const Fixed &first, const Fixed &second);
+	Fixed &operator<=(const Fixed &first, const Fixed &second);
+	Fixed &operator==(const Fixed &first, const Fixed &second);
+	Fixed &operator!=(const Fixed &first, const Fixed &second);
+	Fixed &operator+(const Fixed &first, const Fixed &second);
+	Fixed &operator-(const Fixed &first, const Fixed &second);
+	Fixed &operator*(const Fixed &first, const Fixed &second);
+	Fixed &operator/(const Fixed &first, const Fixed &second);
+	Fixed &operator++(const Fixed &other);
+	Fixed &operator--(const Fixed &other);
+
+private:
+	int					_value;
+	static const int	_fract_bits = 8;
+
+};
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
+int	ft_power(int base, int exp);
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 13:37:52 by ipuig-pa          #+#    #+#             */
+/*   Updated: 2025/02/06 10:56:12 by ipuig-pa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+int main( void ) 
+{
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max( a, b ) << std::endl;
+
+	return 0;
+}
